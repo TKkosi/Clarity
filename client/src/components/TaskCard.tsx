ac9a95913396469fae934bbc52435aa670d495f9
Ekwuno kosi <kosisochukwu13@yahoo.com>
2024-11-02 21:19:38 +0100

    navbar

--- client/src/components/TaskCard.tsx
+++ client/src/components/TaskCard.tsx
@@ -1,6 +1,15 @@
  function TaskCard (){
     return (
-        <div></div>
+        <div>
+            <h2>Task Card</h2>
+            <div>
+                <h3>Task Title</h3>
+                
+                <p>Task Description</p>
+                <p>Task Due Date</p>
+                <p>Task Status</p>
+            </div>
+        </div>
     )
  }
 
