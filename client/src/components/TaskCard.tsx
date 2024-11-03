d502405573f3119d0998335e7a664d0595155a31
Ekwuno kosi <kosisochukwu13@yahoo.com>
2024-11-04 00:09:44 +0100

    taskcard

--- client/src/components/TaskCard.tsx
+++ client/src/components/TaskCard.tsx
@@ -1,17 +1,29 @@
- function TaskCard (){
-    return (
-        <div>
-            <h2>Task Card</h2>
-            <div>
-                <h3>Task Title</h3>
-                <input type="text" placeholder="title"/>
-                <p>Task Description</p>
-                <input type="text" placeholder="description"/>
-                <p>Task Due Date</p>
-                <p>Task Status</p>
-            </div>
-        </div>
-    )
- }
 
-    export default TaskCard
+const Form = () => {
+  return (
+    <form className="bg-white border-emerald-600 w-[90vw] md:w-[50vw] p-6 rounded-lg shadow-md font-mono">
+      <div className="mb-4">
+        <label htmlFor="title" className="block text-gray-700 text-sm font-bold mb-2">
+          Title
+        </label>
+        <input placeholder="Enter title" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="description" className="block text-gray-700 text-sm font-bold mb-2">
+            Description
+        </label>
+        <input placeholder="Enter description" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        <label htmlFor="content" className="block text-gray-700 text-sm font-bold mb-2">
+          Content
+        </label>
+        <textarea rows={5} placeholder="Enter your content" id="content" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" defaultValue={""} />
+      </div>
+      <div className="flex items-center justify-between">
+        <button type="submit" className="bg-emerald-800 text-sm hover:bg-emerald-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Post</button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
+
