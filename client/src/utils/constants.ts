0b7c9ca8fdde076598ba7f1da17939887ba30a23
Ekwuno kosi <kosisochukwu13@yahoo.com>
2024-11-21 16:48:34 +0100

    settings 2

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV =="development"? "http://localhost:5005/api":"https://clarity-ruvv.onrender.com"
+export const API_URL = process.env.NODE_ENV =="development"? "http://localhost:5005/api":"https://clarity-ruvv.onrender.com/api"
